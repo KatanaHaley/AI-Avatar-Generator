71a8c9e369966b3bcdd6a90e10c3c1f276cbf8a0
KatanaHaley <haleykatana@gmail.com>
2023-01-09 10:23:06 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,151 @@
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+
+const Home = () => {
+  // Don't retry more than 20 times
+  const maxRetries = 20;
+  const [input, setInput] = useState('');
+  // Create new state property
+  const [img, setImg] = useState('');
+  // Numbers of retries 
+  const [retry, setRetry] = useState(0);
+  // Number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const generateAction = async (req, res) => {
+    console.log('Generating...');
+
+    if (isGenerating && retry === 0) return;
+
+    // Set loading has started
+    setIsGenerating(true);
+
+    // If this is a retry request, take away retryCount
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    if (response.status === 503) {
+      console.log('Model still loading...');
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      setIsGenerating(false);
+      return;
+    }
+
+    // Set image data into state property
+    setImg(data.image);
+    setIsGenerating(false);
+  };
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        setRetryCount(maxRetries);
+        return;
+      }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
+  return (
+    <div className="root">
+      <Head>
+        <title>Katana Image Generator</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Generate images of Katana in any style</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Harness the power of AI and create fantasy, realistic, 8k images and more. Get creative!</h2>
+            <p>Make sure you enter Katana in your prompt</p>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? 'generate-button loading' : 'generate-button'
+                }
+                onClick={generateAction}
+              >
+                {/* Tweak to show a loading indicator */}
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
+          </div>
+         
+        </div>
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://buildspace.so/builds/ai-avatar"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build with buildspace</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
